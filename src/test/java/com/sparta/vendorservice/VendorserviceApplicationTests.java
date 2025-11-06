75ff4d18652c24e6e371484af5b9270dcba4ba84
fleurnari <fleur_nr@naver.com>
2025-11-06 11:59:11 +0900

    Initial commit

---
+++ src/test/java/com/sparta/vendorservice/VendorserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.vendorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
