04e61cfb211a2eb2f8f8b986ee54b594162eeb08
fleurnari <fleur_nr@naver.com>
2025-11-09 17:38:31 +0900

    add(vendor) : 업체 컨트롤러 추가

---
+++ src/main/java/com/sparta/vendorservice/controller/VendorConrtoller.java
@@ -0,0 +1,69 @@
+package com.sparta.vendorservice.controller;
+
+import com.sparta.vendorservice.dto.request.CreateVendorReqDto;
+import com.sparta.vendorservice.dto.request.DeleteVendorReqDto;
+import com.sparta.vendorservice.dto.request.UpdateVendorReqDto;
+import com.sparta.vendorservice.dto.response.*;
+import com.sparta.vendorservice.global.response.ApiResponse;
+import com.sparta.vendorservice.service.VendorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/Vendors")
+@RequiredArgsConstructor
+public class VendorConrtoller {
+
+    private final VendorService vendorService;
+
+    // 업체 추가
+    @PostMapping
+    public ResponseEntity<ApiResponse<CreateVendorResDto>> createVendor(@RequestBody CreateVendorReqDto request) {
+        CreateVendorResDto response = vendorService.createVendor(request);
+        return ResponseEntity.ok(new ApiResponse<>(response));
+    }
+
+    // 업체 수정
+    @PutMapping("/{VendorId}")
+    public ResponseEntity<ApiResponse<UpdateVendorResDto>> updateVendor(
+            @PathVariable UUID VendorId,
+            @RequestBody UpdateVendorReqDto request
+    ) {
+        UpdateVendorResDto response = vendorService.updateVendor(VendorId, request);
+        return ResponseEntity.ok(new ApiResponse<>(response));
+    }
+
+    // 업체 삭제
+    @DeleteMapping
+    public ResponseEntity<ApiResponse<DeleteVendorResDto>> deleteVendor(
+            @RequestBody DeleteVendorReqDto request
+    ) {
+        DeleteVendorResDto response = vendorService.deleteVendor(request);
+        return ResponseEntity.ok(new ApiResponse<>(response));
+    }
+
+    // 업체 리스트 조회
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<GetVendorPageResDto>>> getVendorPage(
+            @RequestParam(required = false) String searchParam,
+            @PageableDefault(size = 10, sort = { "createdAt", "updatedAt" }, direction = Sort.Direction.ASC) Pageable pageable
+    ) {
+        Page<GetVendorPageResDto> response = vendorService.getVendorPage(searchParam, pageable);
+        return ResponseEntity.ok(new ApiResponse<>(response));
+    }
+
+    // 업체 상세 조회
+    @GetMapping("/{VendorId}")
+    public ResponseEntity<ApiResponse<GetVendorDetailResDto>> getVendorDetail(@PathVariable UUID VendorId) {
+        GetVendorDetailResDto response = vendorService.getVendorDetail(VendorId);
+        return ResponseEntity.ok(new ApiResponse<>(response));
+    }
+
+}
